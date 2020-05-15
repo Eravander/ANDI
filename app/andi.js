41fcc33e47dbb72a375b21022e04d06cf3030f4c
s1191 <danieljudsonblair@gmail.com>
2020-05-15 16:38:52 -0400

    express

--- app/andi.js
+++ app/andi.js
@@ -6,6 +6,7 @@
 // import express from "express";
 // import express, { urlencoded, json, static } from "express";
 var andiVersionNumber = "27.0.4";
+var express = require('../node_modules/express');
 
 //==============//
 // ANDI CONFIG: //
