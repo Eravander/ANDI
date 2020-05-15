9c626bbc87eee4f47329b41a686ee609438cdd0e
s1191 <danieljudsonblair@gmail.com>
2020-05-15 19:59:43 -0400

    rearrange

--- app/server.js
+++ app/server.js
@@ -1,9 +1,11 @@
 var express = require('../node_modules/express/index.js');
 var app = express();
 var PORT = process.env.PORT || 3000;
+var andi = require(andi);
 
 app.get("/", function (req, res) {
     console.log("your mother");
+    andi();
 })
 app.listen(PORT, function () {
 	console.log("App running on port " + PORT + "!");
