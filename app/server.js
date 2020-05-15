470bb50e3d2191deaae6ec9984e4de4fa5f3c993
s1191 <danieljudsonblair@gmail.com>
2020-05-15 19:52:30 -0400

    try again

---
+++ app/server.js
@@ -0,0 +1,10 @@
+var express = require('../node_modules/express/index.js');
+var app = express();
+var PORT = process.env.PORT || 3000;
+
+app.get("/", function (req, res) {
+    console.log("your mother");
+})
+app.listen(PORT, function () {
+	console.log("App running on port " + PORT + "!");
+});
