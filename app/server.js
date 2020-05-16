8f570fa4d0845cbbf457d7702a43a83d95163a9a
s1191 <danieljudsonblair@gmail.com>
2020-05-15 22:58:39 -0400

    commit

--- app/server.js
+++ app/server.js
@@ -1,14 +1,15 @@
 
 
 var express = require('../node_modules/express/index.js');
-var andi = require('./andi');
+// var andi = require('./andi');
 var app = express();
 var PORT = process.env.PORT || 3000;
+app.use(require('./andi'));
 
 
 app.get("/", function (req, res) {
     console.log("your mother");
-    andi.dandy;
+    andi.dandy();
 
 })
 app.listen(PORT, function () {
