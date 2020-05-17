0b154983a81eb4b4eb074371edaced1bc930e6bc
danieljudsonblair <45600951+danieljudsonblair@users.noreply.github.com>
2020-05-17 14:59:36 -0400

    Update andi.js

--- andi/andi.js
+++ andi/andi.js
@@ -563,7 +563,7 @@
 
 			var andiBar = "<section id='ANDI508' tabindex='-1' aria-label='ANDI' style='display:none'>" +
 				"<div id='ANDI508-header'>" +
-				"<h1 id='ANDI508-toolName-heading'><a id='ANDI508-toolName-link' class='ANDI508-sectionJump' href='#' aria-haspopup='dialog' aria-label='ANDI " + andiVersionNumber + "'><span id='ANDI508-module-name' data-andi508-moduleversion=''>&nbsp;</span>LIEUT.</br>DANDI</a></h1>" +
+				"<h1 id='ANDI508-toolName-heading'><a id='ANDI508-toolName-link' class='ANDI508-sectionJump' href='#' aria-haspopup='dialog' aria-label='ANDI " + andiVersionNumber + "'><span id='ANDI508-module-name' data-andi508-moduleversion=''>&nbsp;</span>LIEUT</br>DANDI</a></h1>" +
 				"<div id='ANDI508-moduleMenu-container'>" +
 				moduleButtons +
 				"</div>" +
